d43b60fe5eb378af88cefbea346d33b4a28d6ea3
Mark Rattle <mark.rattle@gmail.com>
2023-03-03 18:03:05 -0500

    really really ridiculously good looking

--- climbunity_app/models.py
+++ climbunity_app/models.py
@@ -14,7 +14,7 @@ class SendType(FormEnum):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
@@ -82,9 +82,9 @@ class Venue(db.Model):
     """Venue model"""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    address = db.Column(db.String(80), nullable=False)
-    open_hours = db.Column(db.String(1000))
-    description = db.Column(db.String(1000))
+    address = db.Column(db.String(200), nullable=False)
+    open_hours = db.Column(db.String(500))
+    description = db.Column(db.String(500))
     booked_appointments = db.relationship('Appointment', back_populates='appointment_venue') # Venue <-1 -- N-> Appointment
 
     def __str__(self):
